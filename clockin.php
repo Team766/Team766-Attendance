cad1e55799548191d03932c1f155ed9b0782158e
pjztam <pjztam@gmail.com>
2014-06-29 02:02:12 -0700

    Created HTML page to utilize clock in functionality

---
+++ clockin.php
@@ -0,0 +1,15 @@
+<?php
+
+/*** *** *** *** *** ***
+* @package Team766-Attendence
+* @file    clockin.php 
+* @start   Jun 29, 2014
+* @author  pjztam
+* @link    attendence.team766.com
+*** *** *** *** *** *** */
+
+?>
+
+<form action='clockin_process.php' method='GET' accept-charset="UTF-8">
+<input autofocus="autofocus" type='text' name='studentid' id='studentid' />
+</form>
---
+++ clockin_process.php
@@ -0,0 +1,16 @@
+<?php
+
+/*** *** *** *** *** ***
+* @package Team766-Attendence
+* @file    clockin_process.php 
+* @start   Jun 29, 2014
+* @author  pjztam
+* @link    attendence.team766.com
+*** *** *** *** *** *** */
+    require_once 'includes/db.class.php';
+    require_once 'includes/main.class.php';
+    $main = new main();
+    $db = new db();
+    $db->addAttendEvent($_GET['studentid'], $main->currentDate());
+    header('Location: clockin.php');
+?>
