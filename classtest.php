84a9b7793feeab46dc561e129d400d9e61ab6ce8
pjztam <pjztam@gmail.com>
2014-06-30 18:36:28 -0700

    created method which sums same day hours

--- classtest.php
+++ classtest.php
@@ -108,7 +108,36 @@ if (1==0) {
     $student = new student(194326);
     $student->createScrubbedDateArray();
 }
+if (1==0) {
+    require_once 'includes/db.class.php';
+    $db = new db();
+    $db->addAttendEvent(194326, '2014-01-08 17:01:01');
+    $db->addAttendEvent(194326, '2014-01-08 21:32:26');
+    $db->addAttendEvent(194326, '2014-01-10 14:25:16');
+    $db->addAttendEvent(194326, '2014-01-10 15:13:59');
+    $db->addAttendEvent(194326, '2014-01-10 17:46:51');
+    $db->addAttendEvent(194326, '2014-01-11 09:54:13');
+    $db->addAttendEvent(194326, '2014-01-11 17:46:32');
+    $db->addAttendEvent(194326, '2014-01-15 14:21:23');
+    $db->addAttendEvent(194326, '2014-01-15 15:15:01');
+    $db->addAttendEvent(194326, '2014-01-15 18:34:25');
+    $db->addAttendEvent(194326, '2014-01-15 19:46:24');
+    $db->addAttendEvent(194326, '2014-01-17 17:23:49');
+    $db->addAttendEvent(194326, '2014-01-17 21:54:57');
+    $db->addAttendEvent(194326, '2014-01-18 09:23:57');
+    $db->addAttendEvent(194326, '2014-01-18 17:01:06');
+    $db->addAttendEvent(194326, '2014-01-22 14:24:05');
+    $db->addAttendEvent(194326, '2014-01-22 15:15:15');
+    $db->addAttendEvent(194326, '2014-01-22 18:15:35');
+    $db->addAttendEvent(194326, '2014-01-24 17:15:09');
+    $db->addAttendEvent(194326, '2014-01-24 21:08:16');
+    $db->addAttendEvent(194326, '2014-01-25 09:58:01');
+    $db->addAttendEvent(194326, '2014-01-25 16:14:35');
+}
 if (1==1) {
+    require_once 'includes/student.class.php';
+    $student = new student(194326);
+    echo var_dump($student->getSumTimeWorkedArray($student->createScrubbedDateArray()));
     
 }
 ?>
--- includes/student.class.php
+++ includes/student.class.php
@@ -35,8 +35,45 @@ class student {
     }
     function createScrubbedDateArray() {
         $date_array = $this->createDateArray();
+      
+        $kill_array = $this->getDuplicateDaysKillArray($date_array);
+        
+        for ($i=0; $i<count($kill_array); $i++) {
+            array_splice($date_array, $kill_array[$i]['iBeginning'], $kill_array[$i]['sameDayLength']);
+            if (isset($kill_array[$i+1])) {
+                $kill_array[$i+1]['iBeginning'] = $kill_array[$i+1]['iBeginning'] - $kill_array[$i]['sameDayLength'];
+            }
+        }
+
+        return $date_array;
+    }    
+    function getSumTimeWorkedArray($date_array) {
+        $hours_worked_array = array();
+        $hours_worked_array_index = 0;       
+        for ($i=0; $i<count($date_array); $i = $i + 2 ) {
+            $time_difference = $date_array[$i]->diff($date_array[$i+1]);
+            $hours_worked_array[$hours_worked_array_index] = array("date" => $date_array[$i]->format('Y-m-d'), "time_difference" => $time_difference);
+            $hours_worked_array_index++;
+        }
+        echo var_dump($hours_worked_array);
+        for ($i=0; $i<count($hours_worked_array); $i++) {
+           if ((isset($hours_worked_array[$i+1])) && $hours_worked_array[$i]['date'] == $hours_worked_array[$i+1]['date']) {
+               $dateZero = new DateTime('00:00');
+               $dateZero2 = new DateTime('00:00');
+               $dateZero->add($hours_worked_array[$i]['time_difference']);
+               $dateZero->add($hours_worked_array[$i+1]['time_difference']);
+               $combinedInterval = $dateZero2->diff($dateZero);
+               $hours_worked_array[$i]['time_difference'] = $combinedInterval;
+               array_splice($hours_worked_array, $i, 1);
+               $i--;
+           }
+        }
+    return $hours_worked_array;
+    }
+    function getDuplicateDaysKillArray($date_array) {
         $kill_array = array();
         $kill_array_index = 0;
+        
         for ($i=0; $i<count($date_array); $i++) {
             $iBeginning = $i;
             $i2=0;
@@ -54,15 +91,12 @@ class student {
                 $kill_array_index++;
             }
         }
-        for ($i=0; $i<count($kill_array); $i++) {
-            array_splice($date_array, $kill_array[$i]['iBeginning'], $kill_array[$i]['sameDayLength']);
-            if (isset($kill_array[$i+1])) {
-                $kill_array[$i+1]['iBeginning'] = $kill_array[$i+1]['iBeginning'] - $kill_array[$i]['sameDayLength'];
-            }
-        }
-        return $date_array;
-    }    
-      
+        
+        return $kill_array;
+        
+        
+    }
+    
     function getID() {
         return $this->student_id;
     }
