3ea20c09f10d8736fd092c3e26d179394ff56828
pjztam <pjztam@gmail.com>
2014-07-20 00:20:41 -0700

    changed .gitignore
    unecessary SQL files and .htaccess files were removed

--- sql/frc-attend.sql
+++ sql/frc-attend.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2014 at 09:25 AM
+-- Generation Time: Jul 20, 2014 at 08:16 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -27,11 +27,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ptolemy_attendance` (
-  `index` int(11) NOT NULL AUTO_INCREMENT,
-  `student-id` int(11) NOT NULL,
-  `student-Attendance` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`index`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `student_attendance` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `hash` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ptolemy_hours`
+--
+
+CREATE TABLE IF NOT EXISTS `ptolemy_hours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time_worked` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -40,10 +56,12 @@ CREATE TABLE IF NOT EXISTS `ptolemy_attendance` (
 --
 
 CREATE TABLE IF NOT EXISTS `ptolemy_members` (
-  `student-id` int(11) DEFAULT NULL,
-  `student-name` varchar(70) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `student-date-registered` datetime DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) DEFAULT NULL,
+  `student_name` varchar(70) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `student_date_registered` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
